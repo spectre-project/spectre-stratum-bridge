4b8f7e0e3ac9ff4c871ba821594b486e0764463a
x100111010 <167847953+x100111010@users.noreply.github.com>
2024-05-07 12:34:10 +0200

    check if nBytes is > 0

--- src/gostratum/stratum_client.go
+++ src/gostratum/stratum_client.go
@@ -43,22 +43,25 @@ func spawnClientListener(ctx *StratumContext, connection net.Conn, s *StratumLis
 type LineCallback func(line string) error
 
 func readFromConnection(connection net.Conn, cb LineCallback) error {
-	deadline := time.Now().Add(5 * time.Second).UTC()
-	if err := connection.SetReadDeadline(deadline); err != nil {
-		return err
-	}
+    deadline := time.Now().Add(5 * time.Second).UTC()
+    if err := connection.SetReadDeadline(deadline); err != nil {
+        return err
+    }
 
-	buffer := make([]byte, 1024)
-	_, err := connection.Read(buffer)
-	if err != nil {
-		return errors.Wrapf(err, "error reading from connection")
-	}
-	buffer = bytes.ReplaceAll(buffer, []byte("\x00"), nil)
-	scanner := bufio.NewScanner(strings.NewReader(string(buffer)))
-	for scanner.Scan() {
-		if err := cb(scanner.Text()); err != nil {
-			return err
-		}
-	}
-	return nil
+    buffer := make([]byte, 1024)
+    nbytes, err := connection.Read(buffer)
+    if nbytes > 0 {
+        buffer = bytes.ReplaceAll(buffer[:nbytes], []byte("\x00"), nil)
+        scanner := bufio.NewScanner(strings.NewReader(string(buffer)))
+        for scanner.Scan() {
+            if err := cb(scanner.Text()); err != nil {
+                return err
+            }
+        }
+    }
+    if err != nil {
+        return errors.Wrapf(err, "error reading from connection")
+    }
+
+    return nil
 }
